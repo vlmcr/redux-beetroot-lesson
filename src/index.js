09609a7bc9dc3ceb398d209a102a9a93eaf78dbc
Vadym Dobrydnyk <vdobrydnyk@lohika.com>
2020-06-11 19:56:57 +0300

    Combine reducers

--- src/index.js
+++ src/index.js
@@ -9,19 +9,44 @@ import App from "./components/App"
 const ADD_TODO = "ADD_TODO";
 const TOGGLE_TODO = "TOGGLE_TODO";
 const ADD_COURSE = "ADD_COURSE";
+const DELETE_TODO = "DELETE_TODO"
 
 const actionAddTodo = todo => ({ type: ADD_TODO,  payload: {todo}});
 const actionToggleTodo = id => ({ type: TOGGLE_TODO, payload: {id} });
 const actionAddCourse = course => ({ type: ADD_COURSE,  payload: {course} });
+const actionDeleteTodo = id => ({
+  type: DELETE_TODO,
+  payload: {id}
+});
 
 // Reducer
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    courses: courses(state.courses, action),
+  }
+}
+
 function todos(state = [], action) {
   const { type, payload } = action;
   switch (type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return [...state, payload.todo];
+    case TOGGLE_TODO:
+      return state.map(todo => todo.id !== payload.id ? todo : {...todo, completed: !todo.completed})
+    case DELETE_TODO:
+      return state.filter(todo => todo.id !== payload.id)
     default:
       return state;
+  }
+}
+
+function courses(state = [], action){
+  const { type, payload } = action;
+  switch (type) {
+    case ADD_COURSE:
+      return [...state, payload.course]
+    default: return state;
   }
 }
 
@@ -50,12 +75,20 @@ function createStore(reducer) {
   }
 }
 
-const store = createStore(todos);
+const store = createStore(app);
 const unsubscribe = store.subscribe((state) => console.log(state))
 
 store.dispatch(actionAddTodo({id: 1, title: "Todo 1", completed: false}));
-store.dispatch(actionAddTodo({id: 1, title: "Todo 2", completed: false}));
+store.dispatch(actionAddTodo({id: 2, title: "Todo 2", completed: false}));
+store.dispatch(actionAddTodo({id: 3, title: "Todo 3", completed: false}));
 
-unsubscribe()
+store.dispatch(actionToggleTodo(1));
 
-store.dispatch(actionAddTodo({id: 1, title: "Todo 3", completed: false}));
+store.dispatch(actionToggleTodo(1));
+
+store.dispatch(actionDeleteTodo(1));
+
+
+store.dispatch(actionAddCourse({id: 1, title: "Course 1"}));
+store.dispatch(actionAddCourse({id: 2, title: "Course 2"}));
+
