58b212f933d6b61fac63744425c337c842b557d3
Vadym Dobrydnyk <vdobrydnyk@lohika.com>
2020-06-18 19:28:02 +0300

    Add courses form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,14 @@
 import React from "react"
 
 import { AsyncLoad, lazyLoad } from "./AsyncLoad"
-import Menu from "./Menu"
-import {Route, Switch} from "react-router-dom"
-import FormBook from "./books/BookForm"
+import CoursesPage from "./courses/CoursesPage"
 
-const BooksPage = AsyncLoad(lazyLoad("./books/BooksPage"));
-const BookForm = AsyncLoad(lazyLoad("./books/BookForm"));
+// const BooksPage = AsyncLoad(lazyLoad("./books/BooksPage"));
+// const BookForm = AsyncLoad(lazyLoad("./books/BookForm"));
 
 const App = props => (
   <div className="container">
-    <Menu/>
-    <div className="row">
-      <Switch>
-        <Route exact path={"/"} component={BooksPage} />
-        <Route exact path={"/add-book"} component={BookForm} />
-        <Route path="/add-book/:id?" component={FormBook} />
-      </Switch>
-    </div>
+    <CoursesPage />
   </div>
 )
 
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,41 @@
-import React from "react"
+import React, {useState} from "react"
+
+const initForm = {
+  title: "",
+}
 
 const CoursesPage = () => {
+  const [form, setForm] = useState(initForm);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setForm(prev => ({...prev, [name]: value}))
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(form);
+  }
+
   return (
     <div className="container mt-5">
       <h1>Courses Page</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            onChange={handleChange}
+            value={form.title}
+            name="title"
+            id="title"
+            type="text"
+            className="form-control"
+          />
+        </div>
+
+        <button className="btn btn-primary">Send</button>
+      </form>
     </div>
   )
 }
